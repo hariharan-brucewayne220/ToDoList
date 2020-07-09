7d625ef4d82d961e9e595f821780292087984b1f
hariharan-brucewayne220 <31664045+hariharan-brucewayne220@users.noreply.github.com>
2020-07-09 12:24:54 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/hariharan/todoflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hariharan.todoflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/tasks_list.dart
@@ -0,0 +1,29 @@
+import 'package:todoflutter/widgets/tasks_tile.dart';
+import 'package:provider/provider.dart';
+import 'package:todoflutter/models/task_data.dart';
+import 'package:flutter/material.dart';
+class TasksList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkboxCallback: (checkboxState) {
+                taskData.updateTask(task);
+              },
+              longPressCallback: () {
+                taskData.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskData.taskCount,
+        );
+      },
+    );
+  }
+}
